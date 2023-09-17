97e8f253787e37a08b33d9569c2b0916b5bed0ae
Boris Romao Antunes <romaoboris@gmail.com>
2023-09-17 18:30:06 -0300

    Initial Commit

---
+++ iTunesAppClone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iTunesAppClone
+//
+//  Created by Boris Romão Antunes on 17/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iTunesAppClone/iTunesAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesAppCloneApp.swift
+//  iTunesAppClone
+//
+//  Created by Boris Romão Antunes on 17/09/23.
+//
+
+import SwiftUI
+
+@main
+struct iTunesAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
